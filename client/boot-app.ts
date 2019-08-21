77c02f3b51eb6fab246cfb5f355a8fecb413aa00
Freesia <freesia@studiofreesia.com>
2019-08-21 23:06:37 +0900

    とりあえず、ビルド通った

--- client/boot-app.ts
+++ client/boot-app.ts
@@ -1,46 +1,53 @@
-import Vue from 'vue';
-const Vuetify = require('vuetify');
-import 'vuetify/dist/vuetify.min.css';
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import "@mdi/font/css/materialdesignicons.min.css"
-import VueRouter from 'vue-router';
-import App from './App';
-import Home from './pages/Home';
-import PC from './pages/PC';
-import PS4 from './pages/PS4';
-import store from './store';
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "@mdi/font/css/materialdesignicons.min.css";
+import VueRouter from "vue-router";
+import App from "./App";
+import Home from "./pages/Home";
+import PC from "./pages/PC";
+import PS4 from "./pages/PS4";
+import store from "./store";
 
-Vue.use(VueRouter)
-Vue.use(Vuetify, {
+Vue.use(VueRouter);
+Vue.use(Vuetify);
+
+const vuetify = new Vuetify({
   theme: {
-    primary: '#604F8E',
-    secondary: '#618573',
+    themes: {
+      dark: {
+        primary: "#604F8E",
+        secondary: "#618573"
+      }
+    }
   }
 });
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   routes: [
-    { path: '/', component: Home },
-    { path: '/pc', component: PC },
-    { path: '/ps4', component: PS4 },
+    { path: "/", component: Home },
+    { path: "/pc", component: PC },
+    { path: "/ps4", component: PS4 }
   ]
 });
 
 new Vue({
   router,
   store,
-  el: '#app',
+  vuetify,
+  el: "#app",
   components: { App },
-  template: '<App/>',
+  template: "<App/>"
 });
 
-if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-  window.addEventListener('load', async () => {
+if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
+  window.addEventListener("load", async () => {
     try {
-      await navigator.serviceWorker.register('/service-worker.js');
+      await navigator.serviceWorker.register("/service-worker.js");
     } catch (error) {
-      console.log('SW registration failed: ', error);
+      console.log("SW registration failed: ", error);
     }
   });
 }
--- src/webpack.config.ts
+++ src/webpack.config.ts
@@ -1,96 +1,106 @@
-import { Configuration, EnvironmentPlugin } from 'webpack';
-import * as HtmlPlugin from 'html-webpack-plugin';
-import * as MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import * as LiveReloadPlugin from 'webpack-livereload-plugin';
-import { GenerateSW } from 'workbox-webpack-plugin';
-import * as ManifestPwaPlugin from 'webpack-pwa-manifest';
-import * as CrittersPlugin from 'critters-webpack-plugin';
-import * as path from 'path';
+import { Configuration, EnvironmentPlugin } from "webpack";
+import * as HtmlPlugin from "html-webpack-plugin";
+import * as MiniCssExtractPlugin from "mini-css-extract-plugin";
+import * as LiveReloadPlugin from "webpack-livereload-plugin";
+import { GenerateSW } from "workbox-webpack-plugin";
+import * as ManifestPwaPlugin from "webpack-pwa-manifest";
+import * as CrittersPlugin from "critters-webpack-plugin";
+import * as path from "path";
 
-const src = path.join(__dirname, '..', 'client');
-const dst = path.join(__dirname, '..', 'app', 'public');
+const src = path.join(__dirname, "..", "client");
+const dst = path.join(__dirname, "..", "app", "public");
 
-const isDev = process.env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === "development";
 console.debug(`dev : ${isDev}`);
 
 const config: Configuration = {
   context: src,
   entry: {
-    'bundle': path.join(src, 'boot-app.ts'),
+    bundle: path.join(src, "boot-app.ts")
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [".ts", ".js"],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
-    },
+      vue$: "vue/dist/vue.esm.js"
+    }
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        loader: 'ts-loader',
+        loader: "ts-loader"
       },
-      { test: /\.html/, loader: 'html-loader?minimize=false' },
+      { test: /\.html/, loader: "html-loader?minimize=false" },
       {
         test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.s(c|a)ss$/,
         use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+              fiber: require("fibers"),
+              indentedSyntax: true // optional
+            }
+          }
         ]
       },
       {
         test: /\.(woff2?|eot|ttf|otf|svg)(\?.*)?$/,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: 'assets/[name].[hash:7].[ext]',
+          name: "assets/[name].[hash:7].[ext]"
         }
       }
     ]
   },
-  devtool: isDev ? 'source-map' : false,
+  devtool: isDev ? "source-map" : false,
   plugins: [
-    new EnvironmentPlugin([
-      'NODE_ENV',
-    ]),
+    new EnvironmentPlugin(["NODE_ENV"]),
     new MiniCssExtractPlugin({
-      filename: '[name].[hash:7].css',
+      filename: "[name].[hash:7].css"
     }),
     new HtmlPlugin({
-      template: path.join(src, 'index.html'),
+      template: path.join(src, "index.html"),
       inject: true
     }),
     new ManifestPwaPlugin({
-      name: 'Smart Home Hub',
-      short_name: 'SmartHomeHub',
-      theme_color: '#604f8e',
-      background_color: '#604f8e',
-      orientation: 'natural',
-      display: 'fullscreen',
+      name: "Smart Home Hub",
+      short_name: "SmartHomeHub",
+      theme_color: "#604f8e",
+      background_color: "#604f8e",
+      orientation: "natural",
+      display: "fullscreen",
       icons: [
         {
-          src: path.join(src, 'assets/SHH.png'),
-          destination: 'assets',
+          src: path.join(src, "assets/SHH.png"),
+          destination: "assets",
           sizes: [96, 128, 192, 256, 384, 512]
         }
       ],
-      inject: true,
-    }),
-    new CrittersPlugin(),
+      inject: true
+    })
+    // TODO : cssの複数行コメントがパースできないっぽい？(https://github.com/jsdom/jsdom/issues/2177)
+    // new CrittersPlugin(),
   ].concat(
-    isDev ? [
-      new LiveReloadPlugin({ appendScriptTag: true }),
-    ] : [
-        new GenerateSW({
-          importWorkboxFrom: 'local',
-          skipWaiting: true,
-          clientsClaim: true,
-        }),
-      ]
+    isDev
+      ? [new LiveReloadPlugin({ appendScriptTag: true })]
+      : [
+          new GenerateSW({
+            importWorkboxFrom: "local",
+            skipWaiting: true,
+            clientsClaim: true
+          })
+        ]
   ),
   output: {
-    filename: '[name].[hash:7].js',
+    filename: "[name].[hash:7].js",
     path: dst,
-    publicPath: '/',
+    publicPath: "/"
   }
-}
+};
 export default config;
