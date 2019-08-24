cc1757e97d7e3cfce8e57048789632eb45d026b0
Freesia <freesia@studiofreesia.com>
2019-08-24 18:22:48 +0000

    なんとなく動いた

--- client/boot-app.ts
+++ client/boot-app.ts
@@ -1,13 +1,79 @@
 import Vue from "vue";
-import vuetify from "./plugins/vuetify";
 import VueRouter from "vue-router";
 import App from "./App";
 import Home from "./pages/Home";
 import PC from "./pages/PC";
 import PS4 from "./pages/PS4";
 import store from "./store";
+import Vuetify, {
+  VApp,
+  VNavigationDrawer,
+  VAppBar,
+  VAppBarNavIcon,
+  VToolbarTitle,
+  VList,
+  VListItem,
+  VListItemTitle,
+  VListItemAction,
+  VListItemContent,
+  VIcon,
+  VToolbar,
+  VContent,
+  VContainer,
+  VBtn,
+  VDataTable,
+  VSnackbar,
+  VLayout,
+  VFlex,
+  VProgressCircular,
+  VFooter
+} from "vuetify/lib";
+import "vuetify/dist/vuetify.min.css";
+import "@mdi/font/css/materialdesignicons.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VAppBar,
+    VAppBarNavIcon,
+    VToolbarTitle,
+    VList,
+    VListItem,
+    VListItemTitle,
+    VListItemAction,
+    VListItemContent,
+    VIcon,
+    VToolbar,
+    VContent,
+    VContainer,
+    VBtn,
+    VDataTable,
+    VSnackbar,
+    VLayout,
+    VFlex,
+    VProgressCircular,
+    VFooter
+  }
+});
 
 Vue.use(VueRouter);
+
+const vuetify = new Vuetify({
+  theme: {
+    themes: {
+      dark: {
+        primary: "#604F8E",
+        secondary: "#618573"
+      },
+      light: {
+        primary: "#604F8E",
+        secondary: "#618573"
+      }
+    }
+  }
+});
 
 const router = new VueRouter({
   mode: "history",
