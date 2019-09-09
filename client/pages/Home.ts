8daf7af7fc0fa64aee2dd449e719fe67af715521
Freesia <freesia@studiofreesia.com>
2019-09-09 22:32:28 +0900

    旧クライアント削除

--- client/pages/Home.ts
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-import Component from 'vue-class-component';
-import TestComponent from '../components/TestComponent';
-
-@Component({ template: require('./Home.html'), components: { TestComponent } })
-export default class Home extends Vue {
-}
