1897d7dfd40dd489b8662109944f3b6097b24ca7
Freesia <freesia@studiofreesia.com>
2019-09-16 00:42:40 +0900

    フォーマット

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,18 +1,18 @@
 import "reflect-metadata";
-import { useExpressServer, useContainer} from "routing-controllers";
-import express from 'express';
-import path from 'path';
-import logger from 'morgan';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
-import history from 'connect-history-api-fallback';
-import compression from 'compression';
-import fs from 'fs';
+import { useExpressServer, useContainer } from "routing-controllers";
+import express from "express";
+import path from "path";
+import logger from "morgan";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import history from "connect-history-api-fallback";
+import compression from "compression";
+import fs from "fs";
 import { Container } from "typedi";
-import PcController from './routes/api/pc';
+import PcController from "./routes/api/pc";
 import ConfigService from "./modules/configService";
 
-const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+const config = JSON.parse(fs.readFileSync("config.json", "utf8"));
 Container.set(ConfigService, new ConfigService(config));
 useContainer(Container);
 const app = express();
@@ -21,8 +21,8 @@ useExpressServer(app, {
   controllers: [PcController]
 });
 
-import * as googleHome from './routes/api/google_home';
-import * as ps4 from './routes/api/ps4';
+import * as googleHome from "./routes/api/google_home";
+import * as ps4 from "./routes/api/ps4";
 googleHome.init(config.googlehome);
 ps4.init(config.ps4);
 
@@ -30,13 +30,13 @@ ps4.init(config.ps4);
 //app.use(favicon(path.join(__dirname,'public','favicon.ico')));
 app.use(compression());
 app.use(history());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/api/googlehome', googleHome.default);
-app.use('/api/ps4', ps4.default);
+app.use("/api/googlehome", googleHome.default);
+app.use("/api/ps4", ps4.default);
 
 export default app;
